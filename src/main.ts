991d896fe9e6d29c6bd657c8b818cacd63c1ca25
postfm <72159645+postfm@users.noreply.github.com>
2024-03-09 21:29:42 +0300

    Добавит определение порта

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const DEFAULT_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env);
 
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || DEFAULT_PORT;
+  await app.listen(port);
 }
 bootstrap();
