5e0eb7088750b4925597f92a44b8989c0ed00ad2
postfm <72159645+postfm@users.noreply.github.com>
2024-03-10 15:01:42 +0300

    Добавит глобальный префикс для приложения

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT || DEFAULT_PORT;
+  app.setGlobalPrefix('fitfriends');
+
   await app.listen(port);
 }
 bootstrap();
